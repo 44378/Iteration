f71d63d3d3aa62f36a908bf57e237717c7614af2
44378 <44378@PC004141.coll.lr.local>
2014-10-20 12:17:02 +0100

    Update

--- Class Ex t1.py
+++ Class Ex t1.py
@@ -1,6 +1,8 @@
+#Jack Scaife
+#20/10/14
 # program to prompt for 8 numbers and report the total to the user
-
+total=0
 for count in range(1,9):
-    num = int(input("Enter number {0} : "))
-
-
+    num = int(input("Enter a number : "))
+    total = total+num
+print(total)
